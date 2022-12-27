f15ecd3678691ec05c3a3abdd5450c0c3bfa85a5
Diana <dianapilat232@gmail.com>
2022-12-27 18:23:43 +0200

    Javascript-2

---
+++ task-2.js
@@ -0,0 +1,6 @@
+ let value = prompt('Яка офіційна назва JavaScript?', '');
+   if (value == 'ESMAScript') {
+    alert('Вірно!');
+   } else {
+    alert('Не знаєте? "ESMAScript"!');
+   }
---
+++ task-3.js
@@ -0,0 +1,9 @@
+let a = prompt('Напишіть будь-яке число','');
+
+if (a > 0){
+    alert(1);
+}else if (a < 0){
+    alert(-1);
+}else{
+    alert(0);
+}
